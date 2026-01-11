bd7cf753a4e9de3ec4c9d0d78d234d8be600337c
JuanPablo <juanpablodiaz2518@gmail.com>
2026-01-11 00:02:00 -0300

    Operadores

---
+++ 02-Operadores.py
@@ -0,0 +1,47 @@
+# Operadores
+
+# Enteros
+
+print(8 + 4) # Suma
+print(8 - 4) # Resta
+print(8 / 4) # Division
+print(8 * 4) # Multiplicacion
+print(10 % 3) # Modulo
+print(10 // 3) # Da un valor aproximado de la division, haciendo que la division siempre sea entero
+print( 2 ** 3 ) # Potencias
+
+# Cadenas
+
+print("Hola" + "Python") # Concatena la cadena
+print("Hola " + str(5)) # Concatenar cadena y numero
+print("Hola " * 5) # Multiplica la cadena x veces
+
+mi_float = 3.1 * 2
+print("Hola " * int(mi_float)) # Multiplicar con floats
+
+# Operadores comparativos
+
+print( 3 > 4 ) # Mayor que
+print( 3 < 4 ) # Menor que
+print( 3 >= 4 ) # Mayor igual que
+print( 3 <= 4) # Menor igual que
+print( 3 == 4 ) # Igual que
+print( 3 != 4) # Diferente que
+print( 3 == 3 < 7 > 5) # Multiples operaciones
+
+
+# Comparativos en base a clasificacion alfabetica
+
+print( "Hola" > "Diego" ) # Mayor que
+print( "Hola" < "Diego" ) # Menor que
+print( "Hola" >= "Diego" ) # Mayor igual que
+print( "Hola" <= "Diego") # Menor igual que
+print( "Hola" == "Diego" ) # Igual que
+print( "Hola" != "Diego" ) # Diferente que
+
+# Operadores logico
+
+print( "Hola" > "Mundo" and 5 > 4 )
+print( "Hola" > "Mundo" or 5 > 4 )
+print( not("Hola") > "Mundo" and 5 > 4 )
+
